fb66dbf152e5985922183226148125e778b1c1e2
Andy Tuttle <andytuttle12@gmail.com>
2017-08-03 23:19:39 -0400

    New Boilerplate and setup for react-native album app.

---
+++ index.ios.js
@@ -0,0 +1,13 @@
+// This is a sample react native app for ios.
+
+import React from 'react';
+import {
+	AppRegistry,
+	Text
+} from 'react-native';
+
+const App = () => (
+	<Text>Some Text</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
